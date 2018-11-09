1851aedce572a61b864225df422913e4966b450c
minmint.cs <minmint.cs@macbook-pro-khxng-minmintcs.local>
2018-11-09 13:35:36 +0700

    Initial Commit

---
+++ 13600171projectmini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  13600171projectmini
+//
+//  Created by minmint.cs on 9/11/2561 BE.
+//  Copyright © 2561 Chamavee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
